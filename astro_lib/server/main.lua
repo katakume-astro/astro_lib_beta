88b6311af5a9c5512e042af3755f4fd341b0606d
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-17 20:28:21 +0100

    Update main.lua

--- astro_lib/server/main.lua
+++ astro_lib/server/main.lua
@@ -1,4 +1,6 @@
 local function ban(source, reason)
+    if reason == nil then print('You have not provided content') return end
+    if source == nil then print('You didnt specify a player') return end
     TriggerEvent("EasyAdmin:banPlayer", source, "astro_lib / (" .. reason .. ")", 1044463300)
     log(source, 'the player was banned for (" .. reason .. ")')
 end

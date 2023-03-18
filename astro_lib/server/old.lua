db8d314744df1ec3be95528f1f54acbb9d1dfc75
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-18 23:23:42 +0100

    Update old.lua

--- astro_lib/server/old.lua
+++ astro_lib/server/old.lua
@@ -1,7 +1,8 @@
 RegisterNetEvent('astro:cheater')
 AddEventHandler('astro:cheater', function()
+local _src = source
 local reason = 'ban'
 local Invoke = GetInvokingResource()
 print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5astro:cheater^7 Event, this event ^1no longer supported^7"):format(Invoke))
-ban(source, reason)
+ban(_src, reason)
 end)

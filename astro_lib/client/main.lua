6ee49b58bd24195cf642f745cd56752eecfda720
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-18 22:55:03 +0100

    Update main.lua

--- astro_lib/client/main.lua
+++ astro_lib/client/main.lua
@@ -20,3 +20,17 @@ end
 
 -- exports['astro_lib']:spawnvehicle('blista', vector3(402.7710, -1633.7021, 29.2919), 258.1375)
 exports('spawnvehicle', spawnvehicle) 
+
+function spawnped(ped, coords, heading)
+        if lib.requestModel(ped, 1000) then 
+            npc = CreatePed(1, GetHashKey(ped), coords, heading, false, false)
+            FreezeEntityPosition(npc, true)
+            SetEntityInvincible(npc, true)
+            SetBlockingOfNonTemporaryEvents(npc, true)
+        else 
+            print('Couldn\'t load npc')
+        end
+end
+    
+-- exports['astro_lib']:spawnped('csb_anita', vector3(402.7710, -1633.7021, 29.2919), 258.1375)
+exports('spawnped', spawnped)

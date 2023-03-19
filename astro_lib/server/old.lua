c627610e8f46fb71c07e80672cf739a73eb5f0dc
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-19 13:18:16 +0100

    Update old.lua

--- astro_lib/server/old.lua
+++ astro_lib/server/old.lua
@@ -1,5 +1,5 @@
 RegisterNetEvent('astro:cheater')
 AddEventHandler('astro:cheater', function()
 local Invoke = GetInvokingResource()
-print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5astro:cheater^7 Event, this event ^1no longer supported!^7 Visit https://github.com/katakume-astro/astro_lib_beta/edit/main/astro_lib/server/old.lua for how to fix!"):format(Invoke))
+print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5astro:cheater^7 Event, this event ^1no longer supported"):format(Invoke))
 end)

30ae0a8704b3e61728508ba65a11dc45be8df84f
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-25 13:33:36 +0100

    Update config.lua

--- astro_lib/config.lua
+++ astro_lib/config.lua
@@ -2,3 +2,4 @@ Config                            = {}
 Config.DiscordWebhook = ""
 Config.TaskWarpPedIntoVehicle = true
 Config.NotifyType = "esx" --esx, mythic_notify, okokNotify
+Config.AddCarKeys = true -- client/main.lua
